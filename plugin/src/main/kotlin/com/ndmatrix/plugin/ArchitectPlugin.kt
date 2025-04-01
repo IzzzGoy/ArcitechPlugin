481ad856432296adcde71762aa771de1d51b85e0
alexskakun <skakun@peterpartner.net>
2025-04-01 17:18:41 +0300

    fix task

--- plugin/src/main/kotlin/com/ndmatrix/plugin/ArchitectPlugin.kt
+++ plugin/src/main/kotlin/com/ndmatrix/plugin/ArchitectPlugin.kt
@@ -5,39 +5,65 @@ import com.ndmatrix.plugin.models.ConfigSchema
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.decodeFromStream
+import org.gradle.api.DefaultTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.internal.project.DefaultProject
 import org.gradle.api.provider.Property
 import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.InputDirectory
+import org.gradle.api.tasks.OutputDirectory
+import org.gradle.api.tasks.PathSensitive
+import org.gradle.api.tasks.PathSensitivity
+import org.gradle.api.tasks.TaskAction
 import kotlin.io.path.div
 import kotlin.io.path.exists
+
 
 abstract class GenerateExtension {
     @get:Input
     abstract val packageName: Property<String>
-
 }
 
 class ArchitectPlugin : Plugin<Project> {
-    @OptIn(ExperimentalSerializationApi::class)
     override fun apply(project: Project) {
         val extension = project.extensions.create("architect", GenerateExtension::class.java)
 
-        project.tasks.create("generate") {
-            doLast {
-                val configFolder = arrayOf(
-                    project.layout.projectDirectory.asFile.toPath() / "src/commonMain/config",
-                    project.layout.projectDirectory.asFile.toPath() / "src/main/config"
-                ).firstOrNull { it.exists() }
-                if (configFolder == null) {
-                    return@doLast
-                }
-                val configSchema = configFolder.toFile().listFiles()?.first { it.extension == "json" } ?: return@doLast
-                val decodedSchema = Json.decodeFromStream<ConfigSchema>(configSchema.inputStream())
-                ParametersGenerator().generate(decodedSchema, extension.packageName.get()).forEach {
-                    it.writeTo((project.layout.buildDirectory.asFile.get().toPath() / "generated/architect"))
-                }
-            }
+        project.tasks.register("generate", GenerateTask::class.java) {
+            this.packageName.set(extension.packageName)
+            this.configDirectory.set(
+                project.layout.projectDirectory.dir("src/commonMain/config")
+            )
+            this.generatedOutputDir.set(
+                project.layout.buildDirectory.dir("generated/architect")
+            )
+        }
+    }
+}
+
+abstract class GenerateTask : DefaultTask() {
+    @get:Input
+    abstract val packageName: Property<String>
+
+    @get:InputDirectory
+    @get:PathSensitive(PathSensitivity.RELATIVE)
+    abstract val configDirectory: DirectoryProperty
+
+    @get:OutputDirectory
+    abstract val generatedOutputDir: DirectoryProperty
+
+    @TaskAction
+    fun generateFiles() {
+        val configFolder = configDirectory.get().asFile
+        val configSchema = configFolder.listFiles()?.firstOrNull { it.extension == "json" }
+            ?: return
+
+        val decodedSchema = Json.decodeFromStream<ConfigSchema>(configSchema.inputStream())
+        val outputDir = generatedOutputDir.get().asFile.toPath()
+
+        ParametersGenerator().generate(decodedSchema, packageName.get()).forEach {
+            it.writeTo(outputDir)
         }
     }
 }
