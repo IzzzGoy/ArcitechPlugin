372c653ac7ab790c006460893f82afaed626414e
alexskakun <skakun@peterpartner.net>
2025-04-04 20:18:48 +0300

    fix add custom repo

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -17,7 +17,7 @@ java {
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
     implementation("com.squareup:kotlinpoet:2.1.0")
-    implementation("com.ndmatrix.parameter:library:1.0.1")
+    implementation("io.github.izzzgoy:ndimmatrix:1.0.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
 }
 
